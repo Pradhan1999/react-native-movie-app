d1d6ecab35b1d60988976484fbcea44cceb73089
Deepak <pradhan171219@gmail.com>
2025-07-03 13:48:39 +0530

    Add AnimeCard component and integrate it into Home screen for displaying anime list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import AnimeCard from "@/components/AnimeCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
@@ -5,7 +6,14 @@ import { fetchAnimes } from "@/services/api";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 const Home = () => {
   const router = useRouter();
@@ -32,16 +40,42 @@ const Home = () => {
             placeholder="Search for a anime"
           />
 
-          <View className="mt-5 border border-red-50">
-            {animes?.map((anime) => (
-              <View
-                key={anime.mal_id}
-                className="bg-white p-4 rounded-lg mb-4 shadow"
-              >
-                <Text className="text-lg font-bold">{anime.title}</Text>
-                <Text className="text-gray-600">{anime.source}</Text>
+          <View className="mt-5">
+            {loading ? (
+              <ActivityIndicator
+                size="large"
+                color="#000ff"
+                className="mt-10 self-center"
+              />
+            ) : error ? (
+              <Text>Error: {error.message}</Text>
+            ) : (
+              <View>
+                <FlatList
+                  data={animes}
+                  renderItem={({ item }) => <AnimeCard {...item} />}
+                  keyExtractor={(item) => item.mal_id.toString()}
+                  numColumns={3}
+                  columnWrapperStyle={{
+                    justifyContent: "flex-start",
+                    gap: 20,
+                    paddingRight: 5,
+                    marginBottom: 10,
+                  }}
+                  className="mt-2 pb-32"
+                  scrollEnabled={false}
+                />
+                {/* {animes?.map((anime) => (
+                  <View
+                    key={anime.mal_id}
+                    className="bg-white p-4 rounded-lg mb-4 shadow"
+                  >
+                    <Text className="text-lg font-bold">{anime.title}</Text>
+                    <Text className="text-gray-600">{anime.source}</Text>
+                  </View>
+                ))} */}
               </View>
-            ))}
+            )}
           </View>
         </View>
       </ScrollView>
